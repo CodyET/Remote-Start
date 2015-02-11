29bbba7d955d63f20bb25005fce963dad2df57c0
Harrison Jones <harrisonhjones@gmail.com>
2015-02-11 14:41:18 -0800

    Removed daily time sync
    Since the core is going to deep sleep every day at UTC = 5 the time doesn't need to be manually synced every day

--- application.cpp
+++ application.cpp
@@ -46,18 +46,12 @@ void setup()
     // Start the time sync & wait until it actually syncs. 
     Spark.syncTime();
     while(Time.year() == 1970) {  SPARK_WLAN_Loop(); }
+    lastSync = millis();
 }
 
 /* This function loops forever --------------------------------------------*/
 void loop()
 {
-    
-    if (millis() - lastSync > ONE_DAY_MILLIS) {
-        // Request time synchronization from the Spark Cloud
-        Spark.syncTime();
-        lastSync = millis();
-      }
-  
     if(Serial.print(Time.hour())==5)
     {
         Spark.sleep(SLEEP_MODE_DEEP,27000);
